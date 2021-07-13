a83610cfbab2dd7dc7401badb71fec833065f836
Sean McPherson <git@seanmcp.com>
2021-07-13 06:44:03 -0400

    feat: build background.ts

---
+++ src/background.ts
@@ -0,0 +1,3 @@
+console.log('SVEX up and running!')
+
+export {}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+// See https://stackoverflow.com/a/50052194
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [svelte()]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "src", "background.ts"),
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        // Doesn't currently work, see https://github.com/vitejs/vite/issues/2944
+        assetFileNames: `assets/[name].[ext]`
+      }
+    },
+  },
+  plugins: [svelte()],
+});
