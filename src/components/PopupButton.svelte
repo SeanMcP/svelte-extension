a23e84e8f8d57c19107a385f7442386412617485
Sean McPherson <git@seanmcp.com>
2021-08-28 06:59:03 -0400

    fix: open popup from any page

--- src/components/PopupButton.svelte
+++ src/components/PopupButton.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import browser from "webextension-polyfill";
+  import { BASE_PATH } from "../constants";
 
   function handleClick() {
     const bodyRect = document.querySelector("body").getBoundingClientRect();
 
     browser.windows.create({
-      url:
-        window.location.href +
-        (window.location.href.includes("?") ? "&" : "?") +
-        "popup=true",
+      url: BASE_PATH + '?popup=true', // History will redirect you to the correct url
       type: "popup",
       width: Math.round(bodyRect.width ? bodyRect.width : 360),
       height: Math.round(bodyRect.height ? bodyRect.height + 30 : 400),
